21fc5f486eeb5bfed1f960d7d7f28a49873a799c
devjamie <dev.j.pewu@gmail.com>
2025-01-29 20:58:46 -0500

    implemented add ()

--- src/Demo1/Demotest5.java
+++ src/Demo1/Demotest5.java
@@ -5,6 +5,17 @@ public class Demotest5 {
 	public static void main(String[] args) {
 		
 		System.out.println("Java");
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a+b;
+		
+		System.out.println(result);
 	}
 
+	
+	public void add (int a, int b)  {
+		System.out.println(a+b);
+	}
 }
